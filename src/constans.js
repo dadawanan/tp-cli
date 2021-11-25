debdb537d0a226a967310807931c469f0ca6f2da
dadawanan <h1159827456@outlook.com>
2021-11-25 18:46:41 +0800

    初次提交

--- src/constans.js
+++ src/constans.js
@@ -2,7 +2,9 @@
 const { version } = require('../package.json');
 
 // 存储模板的位置 下载前先找临时目录存放下载的文件
-const downLoadDirectory = `${process.env[process.platform === 'darwin' ? 'HOME' : 'USERPROFILE']}\\.template`;
+// console.log(process.platform);
+const downLoadDirectory = `${process.env[process.platform === 'darwin' ? 'HOME' : 'USERPROFILE']
+  }/.template`;
 // console.log(downLoadDirectory);
 module.exports = {
   version,
